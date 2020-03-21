08ff97bc67c3467ea6f438eab448f9397b57ff24
Will Haslett <>
2020-03-20 20:31:44 -0400

    Home page stub in place

---
+++ lib/home.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(centerTitle: true, title: Text('CovidLocal')),
+      body: Text('foo')
+    );
+  }
+}
